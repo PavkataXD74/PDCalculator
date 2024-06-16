bd07b060048ab52cdaac33079fbbc7db892da384
PavkataXD74 <pavel1sou@abv.bg>
2024-06-16 18:23:29 +0300

    validateEquation
    added validate function

--- calculator.c
+++ calculator.c
@@ -108,12 +108,95 @@ void tokenize(char* line, Stack* operands, Stack* operators)
             addToStack(operators, "log");
         }
         else if(newLine[i] != '\0'){
-            printf("Invalid character detected!\n");
+            printf("Invalid syntax!\nAt character number %d:\n", i+1);
+            printf("Unrecognized \"%c\"\n", newLine[i]);
+            printf("%s\n", line);
+            while(i > 0)
+            {
+                i--;
+                printf(" ");
+            }
+            printf("^\n");
             exit(1);
         }
     }
 
     free(newLine);
+}
+
+int isValidNumber(char* number){
+    if(number[0] == '.') return 0;
+    int lettersInNumber = 0;
+    int afterDot = 0;
+    int dotCount = 0;
+    for(int i = 0; i < strlen(number); i++){
+        if(number[i] == '.'){
+            dotCount ++;
+            if(dotCount > 1){
+                return 0;
+            }
+        }else if(number[i] <= '9' && number[i] >= '0' && dotCount == 1){
+            afterDot = 1;
+        }else if(number[i] <= '9' && number[i] >= '0'){
+            continue;
+        }else{
+            lettersInNumber = 1;
+            break;
+        }
+    }
+    // printf("dotCount: %d; afterDot: %d; lettersInNumber:%d\n", dotCount, afterDot, lettersInNumber);
+    if((dotCount == 1 && afterDot == 1 && lettersInNumber == 0) || (dotCount == 0 && afterDot == 0 && lettersInNumber == 0)){
+        return 1;
+    }
+    return 0;
+}
+
+int validateEquation(Stack* operands, Stack* operators){
+    int operandsCount = operands->size;
+    int operatorsCount = operators->size;
+
+    struct stackNode* currentNode = operands->stackHead;
+    int treversedNodes = 0;
+    while(currentNode != NULL){
+        int flag = isValidNumber(currentNode->value);
+        if(flag == 0){
+            return treversedNodes;
+        }else if(flag == 1){
+            treversedNodes++;
+            currentNode = currentNode->lastElem;
+        }
+    }
+
+    currentNode = operators->stackHead;
+    while(currentNode != NULL){
+        if(strcmp(currentNode->value, "+") == 0 || 
+           strcmp(currentNode->value, "-") == 0 || 
+           strcmp(currentNode->value, "*") == 0 || 
+           strcmp(currentNode->value, "/") == 0 || 
+           strcmp(currentNode->value, "log") == 0)
+        {
+            operandsCount--;
+            operatorsCount--;
+        }else if(strcmp(currentNode->value, "ln") == 0){
+            operatorsCount--;
+        }else{
+            printf("Invalid operation!\n");
+            exit(1);
+        }
+        currentNode = currentNode->lastElem;
+    }
+    if(operandsCount == 1 && operatorsCount == 0){
+        printf("Valid Equation\n");
+        return -1;
+    }else{
+        printf("Invalid Equation! ");
+        if(operandsCount > operatorsCount){
+            printf("Not enough operators!\n"); 
+        }else if(operandsCount <= operatorsCount){
+            printf("Too many operators!\n"); 
+        }
+        return -2;
+    }
 }
 
 
@@ -136,21 +219,32 @@ int main()
     tokenize(line, operands, operators);
 
 
-    int operandsSize = operands->size;
-    int operatorsSize = operators->size;
-    for(int i=0; i<operandsSize; i++)
-    {
-        printf("%s\n", takeFromStack(operands));
-    }
-    printf("\n");
-    for(int i=0; i<operatorsSize; i++)
-    {
-        printf("%s\n", takeFromStack(operators));
-    }
+    // int operandsSize = operands->size;
+    // int operatorsSize = operators->size;
+    // for(int i=0; i<operandsSize; i++)
+    // {
+    //     printf("%s\n", takeFromStack(operands));
+    // }
+    // printf("\n");
+    // for(int i=0; i<operatorsSize; i++)
+    // {
+    //     printf("%s\n", takeFromStack(operators));
+    // }
 
-
-    free(operands);
-    free(operators);
+    int errorFlag = validateEquation(operands, operators);
+    if(errorFlag == -2){
+        return 1;
+    }else if(errorFlag == -1){
+        //calculate
+    }else if(errorFlag >= 0){
+        struct stackNode* currentNode = operands->stackHead;
+        for(int i = 0; i < errorFlag; i++){
+            currentNode =currentNode->lastElem;
+        }
+        printf("Your equation: %s\n", line);
+        printf("Invalid operator at: %s\n", currentNode->value);
+        return 1;
+    }
 
     return 0;
 }
